4301902be2616c42c3a9de46ba3beda716a984b2
Peter Demin <peterdemin@gmail.com>
2023-02-02 20:39:05 -0500

    Migrate to Mailchimp

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,20 +1,58 @@
-require('dotenv').config()
-const fetch = require('node-fetch')
-const { EMAIL_TOKEN } = process.env
-exports.handler = async event => {
-  const email = JSON.parse(event.body).payload.email
-  console.log(`Recieved a submission: ${email}`)
-  return fetch('https://api.buttondown.email/v1/subscribers', {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${EMAIL_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Submitted to Buttondown:\n ${data}`)
-    })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+const mailchimp = require('@mailchimp/mailchimp_marketing')
+const crypto = require('crypto')
+
+const { MAILCHIMP_API_KEY, MAILCHIMP_SERVER_PREFIX, MAILCHIMP_LIST_ID } = process.env
+
+mailchimp.setConfig({
+  apiKey: MAILCHIMP_API_KEY,
+  server: MAILCHIMP_SERVER_PREFIX,
+})
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+}
+
+exports.handler = async function(event, context) {
+  // Hello our dear friend, CORS. Nice to see you.
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 204,
+      headers
+    }
+  }
+
+  const { email } = JSON.parse(event.body)
+  const subscriberHash = crypto.createHash('md5').update(email).digest('hex')
+
+  try {
+    const response = await mailchimp.lists.setListMember(
+      MAILCHIMP_LIST_ID,
+      subscriberHash,
+      {
+        email_address: email,
+        status_if_new: 'pending',
+      },
+      { skipMergeValidation: true }
+    )
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        status: response.status,
+        email: response.email_address,
+      })
+    }
+  } catch (e) {
+      return {
+        statusCode: 400,
+        headers,
+        body: JSON.stringify({
+          status: 'error',
+          error: e.response.body.title,
+        })
+      }
+  }
 }
